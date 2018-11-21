cd4f651ace6514e58ff00f6ae7735b83ead574d1
pedro <pedroluismontesinosrivera@gmail.com>
2018-11-21 18:26:31 -0500

    api is returning whatever images I want

--- src/App.js
+++ src/App.js
@@ -2,11 +2,19 @@ import React, { Component } from 'react';
 import './App.css';
 
 import SearchBar from './components/SearchBar';
+import axios from 'axios';
+
+const API_KEY = `${process.env.REACT_APP_API_KEY}`;
 
 class App extends Component {
 
   formSubmit(search){
-    console.log(search);
+    axios.get('https://api.unsplash.com/search/photos', {
+      params: { query: search },
+      headers: {
+        Authorization: `Client-ID ${API_KEY}`
+      }
+    })
   }
 
   render() {
