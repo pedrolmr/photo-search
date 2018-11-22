4c0722e10885b470b9d5bd251e93d7eec7063a53
pedro <pedroluismontesinosrivera@gmail.com>
2018-11-21 19:14:41 -0500

    working on displaying images

--- src/App.js
+++ src/App.js
@@ -7,20 +7,23 @@ import axios from 'axios';
 const API_KEY = `${process.env.REACT_APP_API_KEY}`;
 
 class App extends Component {
+  state = { images: [] }
 
-  formSubmit(search){
-    axios.get('https://api.unsplash.com/search/photos', {
+  formSubmit = async (search) => {
+    const response = await axios.get('https://api.unsplash.com/search/photos', {
       params: { query: search },
       headers: {
         Authorization: `Client-ID ${API_KEY}`
       }
     })
+    this.setState({images: response.data.results});
   }
 
   render() {
     return (
       <div className="App">
         <SearchBar onSubmit={this.formSubmit}/>
+        Found: {this.state.images.length} images
       </div>
     );
   }
