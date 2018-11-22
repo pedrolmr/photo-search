b37d9d6620e204a921c5a82bdda637497ddbd75e
pedro <pedroluismontesinosrivera@gmail.com>
2018-11-21 19:23:20 -0500

    refactored unsplash api

--- src/App.js
+++ src/App.js
@@ -2,19 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 
 import SearchBar from './components/SearchBar';
-import axios from 'axios';
-
-const API_KEY = `${process.env.REACT_APP_API_KEY}`;
+import unsplash from './api/unsplash';
 
 class App extends Component {
   state = { images: [] }
 
-  formSubmit = async (search) => {
-    const response = await axios.get('https://api.unsplash.com/search/photos', {
-      params: { query: search },
-      headers: {
-        Authorization: `Client-ID ${API_KEY}`
-      }
+  formSubmit = async search => {
+    const response = await unsplash.get('/search/photos', {
+      params: { query: search }
     })
     this.setState({images: response.data.results});
   }
